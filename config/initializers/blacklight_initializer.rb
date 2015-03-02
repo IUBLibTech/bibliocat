0e3ab8928255bb2ac2fbfd83ced1f09afabe8e51
Randall Floyd <rdfloyd@indiana.edu>
2015-03-02 17:05:44 -0500

    BIB-6: Initial commit of app with Worthwhile gem installed and functioning

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '8b76344621d57b0c0a4bfbbc5d95af922c54d1e950d50f33d180e8d6017bf5e099db3a7c9db9caa07780776db99c66c0b14e885c228865533bc3df36e29ba3df'
+
