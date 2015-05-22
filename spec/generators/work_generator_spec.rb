2a706df3c2a06c027152dd14b4e8a472d862ef49
Randall Floyd <rdfloyd@indiana.edu>
2015-05-22 15:23:00 -0400

    BIB-93 Adding existence tests for generated files

--- spec/generators/work_generator_spec.rb
+++ spec/generators/work_generator_spec.rb
@@ -13,44 +13,56 @@ describe Bibliocat::WorkGenerator, :type => :generator do
   end
   
     describe 'generated files' do
+      let (:files) { 
+        {controller:'app/controllers/curation_concern/new_types_controller.rb',
+         view: 'app/views/curation_concern/new_types/_new_type.html.erb',
+         actor: 'app/actors/curation_concern/new_type_actor.rb',
+         model: 'app/repository_models/new_type.rb',
+         locale: 'config/locales/new_type.en.yml',
+         controller_spec: 'spec/controllers/curation_concern/new_types_controller_spec.rb',
+         actor_spec: 'spec/actors/curation_concern/new_type_actor_spec.rb',
+         spec: 'spec/repository_models/new_type_spec.rb' }
+      }
+
       before do
         run_generator %w(NewType)
       end
+
       describe 'has a model' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:model]) }
         it { is_expected.to exist }
         it { is_expected.to contain /class NewType/ }
         it { is_expected.to have_correct_syntax }
       end
       describe 'has a controller' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:controller]) }
         it { is_expected.to exist }
-        it { is_expected.to contain /class NewType/ }
+        it { is_expected.to contain /NewTypesController/ }
         it { is_expected.to have_correct_syntax }
       end
       describe 'has an actor' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:actor]) }
         it { is_expected.to exist }
-        it { is_expected.to contain /class NewType/ }
+        it { is_expected.to contain /class NewTypeActor/ }
         it { is_expected.to have_correct_syntax }
       end
       describe 'has views' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:view]) }
         it { is_expected.to exist }
-        it { is_expected.to contain /class NewType/ }
+        #it { is_expected.to contain /class NewType/ }
         it { is_expected.to have_correct_syntax }
       end
       describe 'has specs' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:spec]) }
         it { is_expected.to exist }
-        it { is_expected.to contain /class NewType/ }
+        #it { is_expected.to contain /class NewType/ }
         it { is_expected.to have_correct_syntax }
       end
       describe 'has metadata configuration' do
-        subject { file('app/repository_models/new_type.rb') }
+        subject { file(files[:locale]) }
         it { is_expected.to exist }
-        it { is_expected.to contain /class NewType/ }
-        it { is_expected.to have_correct_syntax }
+        #it { is_expected.to contain /class NewType/ }
+        it 'is_expected.to have_correct_syntax'
       end
       describe 'is a registered concern'
       describe 'has metadata methods matching the configuration'
