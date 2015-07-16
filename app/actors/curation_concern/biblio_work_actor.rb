59158ab2dff1827f33d7945cc2b6df7efc31bafc
Daniel Pierce <dlpierce@indiana.edu>
2015-07-16 10:49:08 -0400

    Update a secondary solr index

--- app/actors/curation_concern/biblio_work_actor.rb
+++ app/actors/curation_concern/biblio_work_actor.rb
@@ -2,5 +2,24 @@
 #  `rails generate worthwhile:work BiblioWork`
 module CurationConcern
   class BiblioWorkActor < CurationConcern::GenericWorkActor
+
+    def create
+      super
+
+    end
+
+    def update
+      super
+
+    end
+
+    def save
+      super
+      Thread.new do
+        ActiveFedora::SolrService.register('http://localhost:8983/solr/spotlight')
+        curation_concern.update_index
+      end
+    end
+
   end
 end
