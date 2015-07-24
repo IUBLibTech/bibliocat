fefb88f83d41ee3ab587afe54ea43d72b183b9d7
Randall Floyd <rdfloyd@indiana.edu>
2015-07-24 16:23:51 -0400

    BIB-97: Adding logic in RDF datastream class to dynamically add properties matching vocab config

--- app/repository_models/datastreams/biblio_work_metadata.rb
+++ app/repository_models/datastreams/biblio_work_metadata.rb
@@ -1,3 +1,21 @@
 class BiblioWorkMetadata < ActiveFedora::NtriplesRDFDatastream
   include BiblioWorkRdfProperties
+
+  #TODO Here we need to walk across vocabularies unique to the work type
+  # We will check first to hopefully find the named vocab in RDF:: or RDF::Vocab.
+  # Failing that we will create a vocab definition on the fly, providing we have a URI
+
+
+
+  # We need a way to check for classes that can return methods for our vocab names
+  def class_send(class_name, method, *args)
+    return nil unless Object.const_defined?(class_name)
+    c = Object.const_get(class_name)
+    c.respond_to?(method) ? c.send(method, *args) : nil
+  end
+
+  # FIXME We won't always drive vocabs with locales but for now we need a helper for sanity checks
+  def i18n_set? key
+      I18n.t key, :raise => true rescue false
+  end
 end
