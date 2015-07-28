0d43cc08dbd6c96ae3eb75bdcf4a11f9a83b848f
Randall Floyd <rdfloyd@indiana.edu>
2015-07-28 08:56:42 -0400

    BIB-97: Rearranging where descMetadata gets defined so each work type can add its own properties to it

---
+++ app/repository_models/datastreams/televised_opera_metadata.rb
@@ -0,0 +1,42 @@
+class TelevisedOperaMetadata < ActiveFedora::NtriplesRDFDatastream
+  include BiblioWorkRdfProperties
+
+  #TODO Here we need to walk across vocabularies unique to the work type
+  # We will check first to hopefully find the named vocab in RDF:: or RDF::Vocab.
+  # Failing that we will create a vocab definition on the fly, providing we have a URI
+
+    # Map properties from vocabularies
+    work_type = 'Televised Opera' 
+    if self.i18n_set? work_type + '.fields' 
+      vocabs =  I18n.t work_type + '.fields' 
+    else 
+      vocabs =  I18n.t 'Generic Work.fields' 
+    end 
+
+   vocabs.each do |vocab, fields|
+      fields.each do |key, value|
+        #class_send(vocab, method)
+        Object.const_get(vocab.to_s).send(key.to_sym)
+      end
+    end
+
+    property :composer, predicate: RDF::MO.composer do |index|
+      index.as :stored_searchable
+    end
+    property :engineer, predicate: RDF::MO.engineer do |index|
+      index.as :stored_searchable, :facetable
+    end
+
+
+  # We need a way to check for classes that can return methods for our vocab names
+  def class_send(class_name, method, *args)
+    return nil unless Object.const_defined?(class_name)
+    c = Object.const_get(class_name)
+    c.respond_to?(method) ? c.send(method, *args) : nil
+  end
+
+  # FIXME We won't always drive vocabs with locales but for now we need a helper for sanity checks
+  def i18n_set? key
+      I18n.t key, :raise => true rescue false
+  end
+end
