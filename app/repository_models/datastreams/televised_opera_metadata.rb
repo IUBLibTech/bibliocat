a14534902320afef18286ef393338f1cf4cbd492
Randall Floyd <rdfloyd@indiana.edu>
2015-07-30 12:42:47 -0400

    BIB-97: Adding ability to define ad-hoc vocabs by existence of URI in config

--- app/repository_models/datastreams/televised_opera_metadata.rb
+++ app/repository_models/datastreams/televised_opera_metadata.rb
@@ -1,11 +1,10 @@
 class TelevisedOperaMetadata < ActiveFedora::NtriplesRDFDatastream
   include BiblioWorkRdfProperties
 
-  #TODO Here we need to walk across vocabularies unique to the work type
+  # Here we need to walk across vocabularies unique to the work type
   # We will check first to hopefully find the named vocab in RDF:: or RDF::Vocab.
   # Failing that we will create a vocab definition on the fly, providing we have a URI
 
-    # Map properties from vocabularies
     work_type = 'Televised Opera' 
     # FIXME Why doesn't my custom i18n_set method work?
 =begin
@@ -17,26 +16,28 @@ class TelevisedOperaMetadata < ActiveFedora::NtriplesRDFDatastream
 =end
     vocabs =  I18n.t work_type + '.fields' 
 
+    # For every vocab section in the config, take each field and setup a property mapping
     vocabs.each do |vocab, fields|
       unless vocab.to_s == 'RDF::DC'
+        vocab_name, uri = vocab.to_s.split
         fields.each do |key, value|
-          # FIXME Why doesn't my custom class_send method work?
-          #property key.to_sym, predicate: class_send(vocab.to_s,key.to_s) do |index|
-          property key.to_sym, predicate: Object.const_get(vocab.to_s).send(key.to_sym) do |index|
-            index.as :stored_searchable, :facetable
+          # Handle custom vocab with a URI by creating it on the fly
+          if uri
+            this_vocab = RDF::Vocabulary.new(uri)
+            property key.to_sym, predicate: this_vocab[key] do |index|
+              index.as :stored_searchable, :facetable
+            end
+          else
+            # Handle vocabularies available in either RDF:: or RDF::Vocab
+            # FIXME Why doesn't my custom class_send method work?
+            #property key.to_sym, predicate: class_send(vocab.to_s,key.to_s) do |index|
+            property key.to_sym, predicate: Object.const_get(vocab_name).send(key.to_sym) do |index|
+              index.as :stored_searchable, :facetable
+            end
           end
         end
       end
     end
-
-=begin
-    property :composer, predicate: RDF::MO.composer do |index|
-      index.as :stored_searchable
-    end
-    property :engineer, predicate: RDF::MO.engineer do |index|
-      index.as :stored_searchable, :facetable
-    end
-=end
 
   # We need a way to check for classes that can return methods for our vocab names
   def class_send(class_name, method, *args)
