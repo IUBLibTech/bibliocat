6e9e425ba56b8cf60d01fa19cfac0ae4fc68b271
randalldfloyd <rdfloyd@indiana.edu>
2015-08-27 11:33:07 -0400

    BIB-101: Improvements to initializer, including getting full list of all work type config files and refining output for when changes occur.

--- config/initializers/bibliocat.rb
+++ config/initializers/bibliocat.rb
@@ -1,17 +1,31 @@
 # Be sure to restart your server when you modify this file.
 require 'yaml'
 
+# This initializer sets up a watched directory to monitor for changes/additions to work type YAML files.
+# It relies on the config gem to parse configuration files and make hashes available via Settings object,
+# and it relies on the listen gem to watch a directory for changes.
 
-work_type_config = Rails.root.join("config", 'work_types', "bibliowork.yml").to_s
-Settings.add_source!(work_type_config)
+# Inspect config/work_types directory to get initial list of current files to add to work type settings 
+Dir.glob('config/work_types/**/*').reject do |work_type_config|
+  puts "Adding #{work_type_config.to_s} to work type settings"
+  Settings.add_source!(Rails.root.join(work_type_config).to_s)
+end
 Settings.reload!
 puts "Available work types with configuration #{Settings.work_types.keys.inspect}"
 
+# Start a listener to watch the work_types dir for modified, added, or removed files
 listener = Listen.to(Rails.root.join("config", 'work_types')) do |modified, added, removed|
-    puts "Added #{added}"
-  unless added[0].nil? 
-    Settings.add_source!(added[0])
-    puts "Added #{added}"
+  modified.each do |m|
+    puts "Modified #{m}"
+  end
+  added.each do |a|
+    unless a.nil? 
+      Settings.add_source!(a)
+      puts "Added #{a}"
+    end
+  end
+  removed.each do |r|
+    puts "Removed #{r}"
   end
   Settings.reload!
   puts "Available work types with configuration #{Settings.work_types.keys.inspect}"
