96003fc9e64ad5227e41adec5fab4715ab566ac7
Daniel Pierce <dlpierce@indiana.edu>
2015-10-13 16:54:04 -0400

    BIB-177 Print errors from secondary solr indexing thread

--- app/actors/curation_concern/biblio_work_actor.rb
+++ app/actors/curation_concern/biblio_work_actor.rb
@@ -17,10 +17,12 @@ module CurationConcern
     def save
       super
       Thread.new do
-        Settings.work_types.to_hash["#{curation_concern.human_readable_type}.to_sym"][:solr_url]
-        if /^http/ =~ solr_url
+        begin
+          solr_url = Settings.work_types.to_hash[curation_concern.human_readable_type.to_sym][:solr_url]
           ActiveFedora::SolrService.register(solr_url)
           curation_concern.update_index
+        rescue
+          pp $!
         end
       end
     end
